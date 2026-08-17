489de2cc792bc0f57c067f0d682533842f973db5
chaijunzhe <3105002635@qq.com>
2026-08-17 09:52:28 +0800

    Add full slide previews

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ siteNav.querySelectorAll("a").forEach((link) => {
         menuButton.setAttribute("aria-expanded", "false");
     });
 });
+
+const imageDialog = document.querySelector("#imageDialog");
+const dialogImage = imageDialog.querySelector("img");
+const dialogClose = imageDialog.querySelector(".dialog-close");
+
+document.querySelectorAll(".slide-preview").forEach((button) => {
+    button.addEventListener("click", () => {
+        const image = button.querySelector("img");
+        dialogImage.src = image.currentSrc || image.src;
+        dialogImage.alt = image.alt;
+        imageDialog.showModal();
+    });
+});
+
+dialogClose.addEventListener("click", () => {
+    imageDialog.close();
+});
+
+imageDialog.addEventListener("click", (event) => {
+    if (event.target === imageDialog) {
+        imageDialog.close();
+    }
+});
